8e1c979c03e3f381facaf96f67936664b928f172
dontarit <tiw815151@gmail.com>
2025-06-23 21:20:54 +0700

    Change frontend to the present

---
+++ frontend/src/js/app-learnPlace.js
@@ -0,0 +1,249 @@
+// Create streak table
+function changeStrTable() {
+    let row, col
+    const tableStr = document.querySelector('.showTableStr')
+    
+    tableStr.classList.forEach(element => {
+        if (element.substring(0, 3) == "row") {
+            row = parseInt(element.substring(4, 6))
+        }
+        if (element.substring(0, 3) == "col") {
+            col = parseInt(element.substring(4, 6))
+        }
+    });
+    for (let i = 1; i <= row; i++) {
+        let theRow = document.createElement('div')
+        theRow.classList.add(`row-${i}`)
+        theRow.classList.add("tableRow")
+
+        for (let j = 1; j <= col; j++) {
+            let theColumn = document.createElement('div')
+            let input = document.createElement('input')
+            let i = document.createElement('i')
+
+            theColumn.classList.add(`col-${j}`)
+            theColumn.classList.add("tableColumn")
+            input.type = 'button'
+            i.classList.add('fa-solid')
+            i.classList.add('fa-circle-check')
+
+            theColumn.appendChild(input)
+            theColumn.appendChild(i)
+            theRow.appendChild(theColumn)
+        }
+        tableStr.appendChild(theRow)
+    }
+    document.querySelector('.tableColumn').style.width = 'calc(50% / (7 / 1.5))'
+}
+changeStrTable()
+
+
+
+// Button animation on click
+const append_btnAnimate = document.querySelectorAll('.btnAnimate')
+append_btnAnimate.forEach(element => {
+    element.addEventListener('click', () => {
+        element.style.transform = 'translateY(-5%) scale(1.02)'
+        element.transition = 'transform 100ms'
+        setTimeout(() => {
+            element.style.transform = 'translateY(0%) scale(1)'
+        }, 100);
+    })
+});
+
+
+
+// Menu toggle button
+const sideMenu = document.getElementById("sideMenu");
+const menuBtn = document.getElementById("menuBtn");
+const menuBtn_out = document.querySelectorAll(".open-menu .menu-btn-out");
+const menuBtn_in = document.querySelector(".open-menu .menu-btn-in")
+const sideItem = document.querySelectorAll('.nav-bar .typeNav div')
+
+function openMenu() {
+    sideMenu.inert = false
+    sideMenu.style.transform = "translateX(0%)";
+    sideMenu.setAttribute("aria-hidden", "false");
+    menuBtn.classList.remove('hdrmnbtn-close')
+    menuBtn.classList.add('hdrmnbtn-open')
+    menuBtn_in.style.transform = 'rotate(90deg)'
+    menuBtn_out.forEach(element => {
+        element.style.opacity = 0
+    });
+    let itemCount = 0
+    sideItem.forEach(element => {
+        setTimeout(() => {
+            element.style.transform = 'translateX(0%)'
+        }, itemCount);
+        itemCount += 50
+    })
+}
+function closeMenu() {
+    sideMenu.inert = true
+    sideMenu.style.transform = "translateX(-100%)";
+    sideMenu.setAttribute("aria-hidden", "true");
+    menuBtn.classList.remove('hdrmnbtn-open')
+    menuBtn.classList.add('hdrmnbtn-close')
+    menuBtn_in.style.transform = 'rotate(0deg)'
+    menuBtn_out.forEach(element => {
+        element.style.opacity = 1
+    });
+    sideItem.forEach(element => {
+        element.style.transform = 'translateX(-50%)'
+    })
+}
+
+menuBtn.addEventListener("click", () => {
+    if (sideMenu.getAttribute('aria-hidden') == 'true') {
+        openMenu()
+    }else {
+        closeMenu()
+    }
+});
+
+document.querySelector('.forceCloseMenu').addEventListener('click', () => {closeMenu()})
+window.addEventListener("click", (e) => {
+    if (
+        sideMenu.getAttribute('aria-hidden') == 'false' &&
+        !sideMenu.contains(e.target) &&
+        e.target !== menuBtn
+    ) {
+        closeMenu();
+    }
+});
+window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && sideMenu.getAttribute('aria-hidden') == 'false') {
+        closeMenu();
+    }
+});
+window.addEventListener('scroll', () => {
+    if (sideMenu.ariaHidden == 'false') {
+        closeMenu()
+    }
+})
+
+
+
+// Search button animation
+const searchBtn = document.querySelectorAll('#activateSearch')
+const searchCon = document.querySelector('.search-container')
+const searchInput = document.getElementById('search-box')
+const headBtn = document.querySelectorAll('.me-hed-btn')
+
+searchBtn.forEach(element => {
+    element.addEventListener('click', () => {
+        if (window.innerWidth < 481) {
+            headBtn.forEach(ele => {
+                ele.style.transition = 'opacity 300ms'
+                ele.style.opacity = '0'
+            });
+        }
+        searchCon.style.transition = 'ease top 300ms'
+        searchCon.style.top = '50%'
+        searchInput.focus()
+    })
+});
+searchInput.addEventListener('focusout', () => {
+    if (window.innerWidth < 481) {
+        headBtn.forEach(element => {
+            element.style.transition = 'opacity 300ms'
+            element.style.opacity = '1'
+        });
+    }
+    searchCon.style.transition = 'ease top 300ms'
+    searchInput.value = ''
+    searchCon.style.top = '-50%'
+})
+
+
+
+// Spin animation for icon
+const setingOpen = document.querySelectorAll('.setingOpen')
+setingOpen.forEach(element => {
+    element.addEventListener('click', () => {
+        let time = 500
+        element.style.transition = `transform ${time}ms`
+        element.style.transform = 'rotate(360deg)'
+    })
+});
+
+
+
+// Change setting option
+const settingBody = document.querySelector('.setting-container')
+const openSetting = document.querySelector('.open-setting')
+const overlaySetting = document.getElementById('overlay-setting-container')
+const closeStBtn = document.querySelector('.setting-container .con-out .ph-x')
+const quesStBtn = document.querySelector('.setting-container .con-out .ph-question-mark')
+
+const slideContent = document.querySelector('.conFor-mainCon')
+const contentOption = document.querySelectorAll('.main-content')
+const topSelect = document.querySelectorAll('.topSelect p')
+
+let option_numberID = ""
+let option_Value = '0'
+
+topSelect.forEach(element => {
+    element.addEventListener('click', () => {
+        option_numberID = element.id
+        let option_Value = parseInt(option_numberID.match(/\d+/)[0])
+        slideContent.style.transform = `translateX(${option_Value * -100}%)`
+        topSelect.forEach(inner => {
+            inner.classList.add('set-at-sub')
+            inner.classList.remove('set-at-main')
+        })
+
+        topSelect[option_Value].classList.add('set-at-main')
+        topSelect[option_Value].classList.remove('set-at-sub')
+        if (option_Value == 0) {
+            contentOption[option_Value].style.opacity = '1'
+            contentOption[option_Value + 1].style.opacity = '0'
+        }
+        else if (option_Value + 1 == topSelect.length) {
+            contentOption[option_Value].style.opacity = '1'
+            contentOption[option_Value - 1].style.opacity = '0'
+        }
+        else {
+            contentOption[option_Value].style.opacity = '1'
+            contentOption[option_Value + 1].style.opacity = '0'
+            contentOption[option_Value - 1].style.opacity = '0'
+        }
+    })
+});
+
+
+
+// Open and Close setting
+openSetting.addEventListener('click', () => {
+    topSelect[0].click()
+    overlaySetting.style.display = 'block'
+    setTimeout(() => {
+        overlaySetting.style.opacity = '1'
+    }, 100);
+    settingBody.classList.remove('setting-container-close')
+    settingBody.classList.add('setting-container-open')
+    settingBody.inert = false
+})
+
+function closeSettingFunc() {
+    overlaySetting.style.opacity = '0'
+    setTimeout(() => {
+        overlaySetting.style.display = 'none'
+    }, 100);
+    openSetting.style.transform = 'rotate(0deg)'
+    settingBody.classList.remove('setting-container-open')
+    settingBody.classList.add('setting-container-close')
+    settingBody.inert = true
+}
+
+const advanceSetting = document.getElementById('advance_setting')
+const submitSetting = document.getElementById('submit_setting')
+const cancleSetting = document.getElementById('cancle_setting')
+const closeSetting = document.querySelector('.setting-container .con-out .ph-x')
+
+cancleSetting.addEventListener('click', () => {
+    closeSettingFunc()
+})
+closeSetting.addEventListener('click', () => {
+    closeSettingFunc()
+})
---
+++ frontend/src/learnPlace.js
@@ -0,0 +1,401 @@
+import { useEffect } from 'react';
+
+import './font/font.css';
+import './css/learnPlace.css';
+import './css/sub/searchbox.css';
+import './css/sub/setting_page.css';
+import './css/sub/waveBtn.css';
+
+import TSLlogo from './img/TSLlogo.png';
+import blankProfile from './img/blank-profile.png';
+// import blankProfile from './img/blank-profile.png';
+import searchBtn from './img/searchBtn.png';
+import favBtn from './img/favBtn.png';
+import handPosBtn from './img/handPosBtn.png';
+import handShapeBtn from './img/handShapeBtn.png';
+import palmTurnBtn from './img/palmTurnBtn.png';
+import daynightBtn from './img/daynightBtn.png';
+import settingBtn from './img/settingBtn.png';
+import mascot from './img/mascot.png';
+
+function LearnPlace() {
+    useEffect(() => { 
+        import('./js/app-learnPlace.js') 
+    }, []);
+    return (
+        <>
+        <header>
+            <div className="con-header">
+                <div className="open-menu me-hed-btn" id="menuBtn">
+                        <span className="menu-btn-out"></span>
+                        <div className="menu-btn-gruop">
+                            <span className="menu-btn-in"></span>
+                            <span className="menu-btn-in"></span>
+                        </div>
+                        <span className="menu-btn-out"></span>
+                </div>
+                <div className="main-logo">
+                    <img src={TSLlogo} alt='logo'/>
+                </div>
+                <div className="search-container">
+                    <div>
+                        <div className="input-place">
+                            <input type="text" name="word" id="search-box" placeholder="Search for..." autoComplete="off"/>
+                        </div>
+                        <div className="free-option">
+                            <div className="clipboard btnAnimate">
+                                <i className="ph ph-clipboard"></i>
+                            </div>
+                            <div className="history btnAnimate">
+                                <i className="ph ph-clock-counter-clockwise"></i>
+                            </div>
+                            <div className="question btnAnimate">
+                                <i className="ph ph-question"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="open-setting me-hed-btn setingOpen">
+                    <i className="ph-fill ph-gear-six" id="settingBtn"></i>
+                </div>
+            </div>
+        </header>
+        <div className="search-result" style={{display: 'none'}}>
+            <div className="history-list">
+                <div className="word-container">
+                    <div>
+                        <p>Hello</p>
+                    </div>
+                    <div>
+                        <p id="meaning">The way to greeting</p>
+                        <p id="group">3</p> 
+                    </div>
+                </div>
+                <div className="word-container">
+                    <div>
+                        <p>Hello</p>
+                    </div>
+                    <div>
+                        <p id="meaning">The way to greeting</p>
+                        <p id="group">3</p> 
+                    </div>
+                </div>
+                <div className="word-container">
+                    <div>
+                        <p>Hello</p>
+                    </div>
+                    <div>
+                        <p id="meaning">The way to greeting</p>
+                        <p id="group">3</p> 
+                    </div>
+                </div>
+                <div className="word-container">
+                    <div>
+                        <p>Hello</p>
+                    </div>
+                    <div>
+                        <p id="meaning">The way to greeting</p>
+                        <p id="group">3</p> 
+                    </div>
+                </div>
+            </div>
+            <div className="search-list"></div>
+        </div>
+        <nav id="sideMenu" aria-hidden="true" role="navigation" aria-label="Side menu" inert>
+            <div className="user-info">
+                <div className="user-container">
+                    <div className="user">
+                        <div className="avatar">
+                            <img src={blankProfile} alt='blank profile'/>
+                        </div>
+                        <div className="name-usetime">
+                            <div className="not-signin" style={{display: 'none'}}>
+                                <p>Haven't signed in yet?</p>
+                                <button className="sign-button .sign-button-hover" role="button">
+                                    <span id="navHead-txt">Sign In</span>
+                                    <div className="liquid"></div>
+                                </button>
+                            </div>
+                            <div className="already-signin">
+                                {/* <p id="navHead-txt">USER NAME</p> */}
+                                <p id="navHead-txt">aaaaaaaaaaaaaaa</p>
+                                <div id="usetime">
+                                    <p>Time usage: </p>
+                                    <p>-</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="stars">
+                        <div className="newStar learn-star">
+                            <img src="img/new star.png"/>
+                            <p id="newSP">0</p>
+                        </div>
+                        <div className="littleStar learn-star">
+                            <img src="img/little star.png"/>
+                            <p id="littleSP">0</p>
+                        </div>
+                        <div className="fullStar learn-star">
+                            <img src="img/full star.png"/>
+                            <p id="fullSP">0</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="nav-bar">
+                <div className="first-nav typeNav">
+                    <div className="searchBtn iconBtn forceCloseMenu" id="activateSearch">
+                        <img src={searchBtn}/>
+                        <p>Search</p>
+                    </div>
+                    <div className="favBtn iconBtn btnAnimate">
+                        <img src={favBtn}/>
+                        <p>Favorite</p>
+                    </div>
+                </div>
+                <div className="second-nav typeNav">
+                    <div className="positionBtn iconBtn btnAnimate">
+                        <img src={handPosBtn}/>
+                        <p>Hand Position</p>
+                    </div>
+                    <div className="shapeBtn iconBtn btnAnimate">
+                        <img src={handShapeBtn}/>
+                        <p>Hand Shape</p>
+                    </div>
+                    <div className="turningBtn iconBtn btnAnimate">
+                        <img src={palmTurnBtn}/>
+                        <p>Palm Turning</p>
+                    </div>
+                </div>
+                <div className="third-nav typeNav">
+                    <div className="daynightBtn iconBtn btnAnimate">
+                        <img src={daynightBtn}/>
+                        <p>Light/Dark</p>
+                    </div>
+                    <div className="settingBtn iconBtn btnAnimate">
+                        <img src={settingBtn}/>
+                        <p>Setting</p>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div className="mainContent-container">
+            <section className="schedule">
+                <div className="tell-streak">
+                    <div className="streak-container stnow">
+                        <p>Current streak</p>
+                        <p id="dayStr">
+                            <span>2</span>
+                            <span>DAY</span>
+                        </p>
+                    </div>
+                    <div className="streak-container stbest">
+                        <p>Best streak</p>
+                        <p id="bestStr">
+                            <span>16</span>
+                            <span>DAY</span>
+                        </p>
+                    </div>
+                </div>
+                <div className="showTableStr row-4 col-7"></div>
+                <div className="tell-history">
+                    <p id="last-use">21/5/2025</p>
+                    <p id="study-time">No History</p>
+                </div>
+            </section>
+            <section className="lvl-review">
+                <div className="charecter-box">
+                    <img src={mascot}/>
+                    <div className="textBox">
+                        <div className="box1">
+                            <div>
+                                <span>You are currently studying</span>
+                            </div>
+                            <div>
+                                <span>134</span>
+                                <span>Thai Sign word.</span>
+                            </div>
+                        </div>
+                        <div className="box2">
+                            <div>
+                                <span>Now you have</span>
+                                <span>10</span>
+                                <span>words</span>
+                            </div>
+                            <div>
+                                <span>ready for review now</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="level-show">
+                    <div className="level-current">
+                        <p>
+                            <span>Level</span>
+                            <span>3-8</span>
+                        </p>
+                        <p>
+                            <span>Next Level:</span>
+                            <span>144</span>
+                            <span>XP</span>
+                        </p>
+                    </div>
+                    <progress id="UserLvl" value="32" max="100"></progress>
+                </div>
+                <div className="progress-word">
+                    <div className="progress-0">
+                        <p id="wordNumber">10</p>
+                        <p>0% - 24%</p>
+                    </div>
+                    <div className="progress-1">
+                        <p id="wordNumber">10</p>
+                        <p>25% - 49%</p>
+                    </div>
+                    <div className="progress-2">
+                        <p id="wordNumber">10</p>
+                        <p>50% - 74%</p>
+                    </div>
+                    <div className="progress-3">
+                        <p id="wordNumber">10</p>
+                        <p>75% - 99%</p>
+                    </div>
+                    <div className="progress-4">
+                        <p id="wordNumber">10</p>
+                        <p><span>---</span>100%<span>---</span></p>
+                    </div>
+                </div>
+                <div className="revBtn_container">
+                    <button id="reviewBtn" className="btn-animate btnAnimate">
+                        <span>START REVIEW</span>
+                        <span>(10)</span>
+                    </button>
+                </div>
+            </section>
+            <section className="Cam-Search">
+                <button className="searchBoxBtn btn-animate search-animate btnAnimate" id="activateSearch">
+                    <p>Search for a word</p>
+                    <i className="ph ph-magnifying-glass"></i>
+                </button>
+                <button className="cameraBoxBtn btn-animate btnAnimate">
+                    <p>Translate with camera</p>
+                    <i className="ph-fill ph-camera"></i>
+                </button>
+            </section>
+            <section className="To-Signin">
+                <div className="Backup-info">
+                    <p>Backup Info</p>
+                    <div>
+                        <span>You are not signed in yet.</span>
+                        <span>Your information will not be saved.</span>
+                    </div>
+                </div>
+                <button className="sign-button" role="button">
+                    <span>Sign In</span>
+                    <div className="liquid"></div>
+                </button>
+            </section>
+        </div>
+        <div className="setting-container">
+            <div className="con-out">
+                <p>Setting</p>
+                <div>
+                    <i className="ph ph-question-mark"></i>
+                    <i className="ph ph-x"></i>
+                </div>
+            </div>
+            <div className="con-in">
+                <div className="topSelect">
+                    <p id="stcon-topic-0" className="set-at-main">Main</p>
+                    <p id="stcon-topic-1" className="set-at-sub">Profile</p>    
+                </div>
+                <div className="conFor-mainCon">
+                    <div className="main main-content">
+                        <section>
+                            <div className="head-group">
+                                <p className="head">Lession</p>
+                                <span className="head-break"></span>
+                            </div>
+                            <div className="content">
+                                <div className="sub-con">
+                                    <p>Weeks of study history</p>
+                                    <input name="history-show" id="history-show" type="text" defaultValue="4 week"/>
+                                </div>
+                                <div className="sub-con">
+                                    <p>Show study streak</p>
+                                    <input name="streak-show" id="streak-show" type="checkbox"/> 
+                                </div>
+                                <div className="sub-con">
+                                    <p>Theme</p>
+                                    <select name="theme" id="theme" defaultValue="dark">
+                                        <option value="light">Light</option>
+                                        <option value="dark">Dark</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </section>
+                        <section>
+                            <div className="head-group">
+                                <p className="head">User Interface</p>
+                                <span className="head-break"></span>
+                            </div>
+                            <div className="content">
+                                <div className="sub-con">
+                                    <p>Dialy study target</p>
+                                    <input type="text" defaultValue="5 minute"/>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    <div className="profile main-content">
+                        <section>
+                            <div className="head-group">
+                                <p className="head">Lession</p>
+                                <span className="head-break"></span>
+                            </div>
+                            <div className="content">
+                                <div className="sub-con">
+                                    <p>Weeks of study history</p>
+                                    <input name="history-show" id="history-show" type="text" defaultValue="4 week"/>
+                                </div>
+                                <div className="sub-con">
+                                    <p>Show study streak</p>
+                                    <input name="streak-show" id="streak-show" type="checkbox"/>
+                                </div>
+                                <div className="sub-con">
+                                    <p>Theme</p>
+                                    <select name="theme" id="theme" defaultValue="dark">
+                                        <option value="light">Light</option>
+                                        <option value="dark">Dark</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </section>
+                        <section>
+                            <div className="head-group">
+                                <p className="head">User Interface</p>
+                                <span className="head-break"></span>
+                            </div>
+                            <div className="content">
+                                <div className="sub-con">
+                                    <p>Dialy study target</p>
+                                    <input type="text" defaultValue="5 minute"/>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+                <div className="bottom-deck">
+                    <input id="advance_setting" type="button" defaultValue="Advance"/>
+                    <div className="inner">
+                        <input id="submit_setting" type="button" defaultValue="Ok"/>
+                        <input id="cancle_setting" type="button" defaultValue="Cancle"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="overlay-setting-container"></div>
+        </>
+    );
+}
+
+export default LearnPlace;
