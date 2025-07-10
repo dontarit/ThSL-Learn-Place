67bec34b1c11d91a2e20dd67e5d4dc62c61e6d64
dontarit <tiw815151@gmail.com>
2025-07-10 21:50:23 +0700

    small change

--- frontend/src/pages/notfound.js
+++ frontend/src/pages/notfound.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function NotFoundPage() {
     return (
         <>
-        <h1>Not Found Page❌</h1>
+        <h1>Page Not Found❌</h1>
         <Link to={"/"}>
-            <button>Go back home</button>
+            <button>Go back</button>
         </Link>
         </>
     )
